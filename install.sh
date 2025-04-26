24c5ecd78d3a43ee9fe69b6e79d8f073fe77adcf
Huang Zhimin <zm-huang@126.com>
2025-04-26 23:14:33 +0800

    openssl-3.0.16 for arm64

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 set -x
-rm $HOME/jgb-build.dir/conf -rf
+rm $HOME/jgb-build.dir/conf.dir -rf
 mkdir -p $HOME/jgb-build.dir
-/bin/cp conf $HOME/jgb-build.dir -r
+/bin/cp conf.dir $HOME/jgb-build.dir -r
