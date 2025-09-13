3464bc78dc74b3133a48ad4e2448d7698cb739e1
Huang Zhimin <zm-huang@126.com>
2025-09-13 15:46:49 +0800

    支持从任意目录执行

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 set -x
+
+script_full_path=$(realpath "$0")
+d=$(dirname $script_full_path)
+cd $d
+PATH=$PWD:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 rm $HOME/jgb-build.dir/conf.dir -rf
 mkdir -p $HOME/jgb-build.dir
 /bin/cp conf.dir $HOME/jgb-build.dir -vr
